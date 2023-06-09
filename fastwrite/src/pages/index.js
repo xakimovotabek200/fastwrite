0578dd88f038150a4a2e3fcdf4c74bf641e27641
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-06-09 08:55:12 +0500

    add

--- fastwrite/src/pages/index.js
+++ fastwrite/src/pages/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 
 function index() {
-<<<<<<< HEAD
-  return (
-    <div>
-      
-    </div>
-  )
-=======
   return <div></div>;
->>>>>>> 93a1bb247cf8206379c86e5c9a59cd6796b8660d
 }
 
 export default index;
