877be9d1e9c9bdd77daecacd680588f1cbdfe608
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-06-11 20:36:46 +0500

    add contact

--- fastwrite/src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import AppLayout from "@/layout/AppLayout";
-import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  const getLayout =
-    Component.getLayout || ((page) => <AppLayout>{page}</AppLayout>);
-  return <>{getLayout(<Component {...pageProps} />)}</>;
-}
